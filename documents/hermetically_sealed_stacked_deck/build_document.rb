9c43e0b61aa67734d9f41126eec648173e61496f
faenrandir <faenrandir@gmail.com>
2021-02-03 09:42:35 -0700

    new or modified

--- documents/hermetically_sealed_stacked_deck/build_document.rb
+++ documents/hermetically_sealed_stacked_deck/build_document.rb
@@ -11,15 +11,20 @@ base_cmd = ["svg_to_pxx.rb"]
 inkscape_files.each do |file|
   cmd = base_cmd + [file]
   final_cmd = cmd.join(" ")
+  puts final_cmd
   system final_cmd
 end
 
 Dir['*.odt'].each do |odt_file|
-  system "odf_to_pdf.rb #{odt_file}"
+  cmd = "odf_to_pdf.rb #{odt_file}"
+  puts cmd
+  system cmd
 end
 
 pdfs = Dir["*.pdf"].reject {|v| v=~ /standalone/}.sort
 
-`pdftk #{pdfs.join(" ")} cat output #{OUTFILE}`
+join_cmd = "pdftk #{pdfs.join(" ")} cat output #{OUTFILE}"
+puts join_cmd
+`#{join_cmd}`
 
 pdfs.each {|file| File.unlink(file) }
