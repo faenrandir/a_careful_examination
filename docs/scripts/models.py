a1d5d3af5ae04271b82921000d718ee6c67cead2
faenrandir <faenrandir@gmail.com>
2021-12-21 16:26:50 -0700

    new or modified

--- docs/scripts/models.py
+++ docs/scripts/models.py
@@ -1,36 +1,181 @@
-#!/usr/bin/env python
+import numpy as np
+import pandas as pd
+from bokeh.layouts import gridplot
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure, output_file, show
+from sklearn.linear_model import LinearRegression
 
-import matplotlib.pyplot as plt
-from numpy.random import normal
-from numpy import arange
-from itertools import chain
-
-
-def parabola(x):
-    return -0.4*(x*x) + 4*x - 6.5
-
-
-xs = list(arange(2.0, 5.5, 0.25))
-rand_xs = [x + normal(scale=0.1) for x in xs]
-rand_ys = [x + normal(scale=0.2) - 1 for x in xs]
+QUADRATIC_COEFFICIENTS = dict(a=-0.4, b=4, c=-6.5)
+X_START = 1.0
+X_END = 10.0
+NUM_POINTS = 200
+ERROR_SCALE = 1.2
 
 
-pxs = list(arange(1.5, 6.5, 0.2))
+class PolyEstimator:
+    """Simple polynomial fit model for sklearn.
 
-prand_xs = [x + normal(scale=0.2) for x in pxs]
-prand_ys = [parabola(x) + normal(scale=0.2) for x in pxs]
+    Credit: https://stackoverflow.com/a/52916579
+    """
 
-# plt.scatter(rand_xs, rand_ys, alpha=0.2)
-# plt.scatter(prand_xs, prand_ys, alpha=0.2)
+    def __init__(self, degree=2):
+        self.degree = degree
 
-plt.scatter(list(chain(rand_xs, prand_xs)), list(chain(rand_ys, prand_ys)), alpha=0.4, color='k')
+    def fit(self, x, y):
+        self.z = np.poly1d(np.polyfit(x.flatten().tolist(), y, self.degree))
 
-xs_line = list(arange(1.25, 5.5, 0.1))
-plt.plot(xs_line, [x - 1 for x in xs_line])
+    def predict(self, x):
+        return self.z(x.flatten().tolist())
 
-xs_para = list(arange(1, 6.7, 0.1))
-plt.plot(xs_para, [parabola(x) + 0.15 for x in xs_para])
 
-plt.xlabel('aspect 1')
-plt.ylabel('aspect 2')
-plt.show()
+def parabola(
+    x,
+    a=QUADRATIC_COEFFICIENTS["a"],
+    b=QUADRATIC_COEFFICIENTS["b"],
+    c=QUADRATIC_COEFFICIENTS["c"],
+):
+    return a * (x ** 2) + (b * x) + c
+
+
+x_values = np.sort(np.random.uniform(X_START, X_END, NUM_POINTS))
+y_values = np.random.normal(list(map(parabola, x_values)), ERROR_SCALE)
+
+# One input variable and len(x_values) observations
+x_matrix = x_values[:, np.newaxis]
+
+model = LinearRegression()
+model.fit(x_matrix, y_values)
+linear_fit_ys = model.predict(x_matrix)
+
+poly = PolyEstimator(degree=2)
+poly.fit(x_matrix, y_values)
+poly_fit_ys = poly.predict(x_matrix)
+
+df = pd.DataFrame(
+    {
+        "x": x_values,
+        "y": y_values,
+        "linear_fit": linear_fit_ys,
+        "quadratic_fit": poly_fit_ys,
+    }
+)
+
+df["linear_fit_error"] = (df.linear_fit - df.y).abs()
+df["quadratic_fit_error"] = (df.quadratic_fit - df.y).abs()
+df["linear_average_error"] = df.linear_fit_error.mean()
+df["quadratic_average_error"] = df.quadratic_fit_error.mean()
+
+ds = ColumnDataSource(df)
+FIG_WIDTH = 400
+RAW_DATA_ALPHA = 0.5
+RAW_DATA_COLOR = "black"
+VBAR_WIDTH = 0.01
+LINEAR_COLOR = "green"
+QUADRATIC_COLOR = "blue"
+FIT_WIDTH = 10.0
+FIT_ALPHA = 0.4
+ERROR_HEIGHT = 250
+MEAN_ERROR_ALPHA = 0.3
+MEAN_ERROR_WIDTH = 5.0
+MAIN_FIG_HEIGHT = 400
+MAIN_FIG_KWARGS = dict(
+    x_axis_label="some variable",
+    height=MAIN_FIG_HEIGHT,
+    width=FIG_WIDTH,
+)
+SCATTER_KWARGS = dict(
+    x="x",
+    y="y",
+    source=ds,
+    color=RAW_DATA_COLOR,
+    alpha=RAW_DATA_ALPHA,
+    legend_label="raw data",
+)
+
+main_raw_fig = figure(**MAIN_FIG_KWARGS, y_axis_label="some response")
+main_raw_fig.scatter(**SCATTER_KWARGS)
+
+main_linear_fig = figure(**MAIN_FIG_KWARGS, x_range=main_raw_fig.x_range)
+main_linear_fig.scatter(**SCATTER_KWARGS)
+main_linear_fig.line(
+    x="x",
+    y="linear_fit",
+    source=ds,
+    color=LINEAR_COLOR,
+    width=FIT_WIDTH,
+    alpha=FIT_ALPHA,
+    legend_label="linear fit",
+)
+
+main_quadratic_fig = figure(**MAIN_FIG_KWARGS, x_range=main_raw_fig.x_range)
+main_quadratic_fig.scatter(**SCATTER_KWARGS)
+main_quadratic_fig.line(
+    x="x",
+    y="quadratic_fit",
+    source=ds,
+    color=QUADRATIC_COLOR,
+    width=FIT_WIDTH,
+    alpha=FIT_ALPHA,
+    legend_label="quadratic fit",
+)
+
+line_error_fig = figure(
+    height=ERROR_HEIGHT,
+    width=FIG_WIDTH,
+    x_range=main_raw_fig.x_range,
+    y_axis_label="error",
+)
+line_error_fig.vbar(
+    x="x",
+    top="linear_fit_error",
+    width=VBAR_WIDTH,
+    source=ds,
+    legend_label="linear fit residuals",
+    color=LINEAR_COLOR,
+)
+line_error_fig.line(
+    x="x",
+    y="linear_average_error",
+    color="red",
+    alpha=MEAN_ERROR_ALPHA,
+    width=MEAN_ERROR_WIDTH,
+    line_dash="dashed",
+    source=ds,
+    legend_label="mean error",
+)
+
+quad_error_fig = figure(
+    height=ERROR_HEIGHT,
+    width=FIG_WIDTH,
+    x_range=main_raw_fig.x_range,
+    y_range=line_error_fig.y_range,
+    y_axis_label="error",
+)
+quad_error_fig.vbar(
+    x="x",
+    top="quadratic_fit_error",
+    width=VBAR_WIDTH,
+    source=ds,
+    color=QUADRATIC_COLOR,
+    legend_label="quadratic fit residuals",
+)
+quad_error_fig.line(
+    x="x",
+    y="quadratic_average_error",
+    color="red",
+    line_dash="dashed",
+    alpha=MEAN_ERROR_ALPHA,
+    width=MEAN_ERROR_WIDTH,
+    source=ds,
+    legend_label="mean error",
+)
+
+combined = gridplot(
+    [
+        [main_raw_fig, main_linear_fig, main_quadratic_fig],
+        [None, line_error_fig, quad_error_fig],
+    ]
+)
+
+output_file("model_fitting.html")
+show(combined)
---
+++ documents/book_of_mormon/oral_revision_example_alma1051.md
@@ -0,0 +1,56 @@
+---
+title: "Bill Davis: Evidence of Oral Revision during the Dictation of the Book of Mormon"
+permalink: /bill-davis-collection-of-evidences-of-oral-revision/
+maintopic: book-of-mormon
+doctype: quotations
+layout: page
+---
+
+## Introduction
+
+The first two examples below were shared by Bill Davis on December 20 and 21 of 2021.
+
+Bill discussed the Alma 10:51 example in some depth.
+
+> Amulek: "I never have known much of the ways of the Lord, and his mysteries and marvelous power. I said I never had known much of these things; but behold, I mistake, for I have seen much of his mysteries and his marvelous power"
+
+> Did you catch that?  The switch from Amulek saying "I never had known" to saying "I mistake, for I have seen."
+
+> It's not a honing or restatement of meaning, but rather a complete reversal from the first claim to the second claim.
+
+> That's what happens when the mouth is running faster than the mind (we all have that problem now and again, right?).
+
+> But this is important to note: This isn't the type of mistake that a person makes who is engraving words on a metal plate.  It's a lot of extra and unnecessary words, and a lot of extra work.
+
+> This also isn't the type of mistake that a person leaves in a text, when revising the draft of a work.  In other words, it's not "refined" (mechanically or, for that matter, stylistically). 
+
+> This is, however, the type of mistake and immediate self-correction and oral revision that occurs in spoken (dictated) scenarios. 
+
+> What this tells us:  When Joseph Smith dictated the Book of Mormon, he was involved in the process as an active contributor to the language of the text.
+
+## Alma 10:51 "I mistake"
+
+*Amulek*: “...I never have known much of the ways of the Lord, and his mysteries and marvelous power. 
+
+*[pause; recognition that a mistake was made]*
+
+I said I never had known much of these things; but behold, I **mistake**, for I ***have*** seen much of his mysteries and his marvelous power ...”
+
+## Alma 52:6 "truly he was preparing"
+
+But he [Teancum] kept his men round about, ***as if*** making preparations for war [pretending];
+
+*[pause; decision to revise]*
+
+yea, and ***truly he was*** preparing [not pretending] to defend himself against
+them, by casting up walls round about and preparing places of resort.
+
+## Alma 24:19 "buried their weapons of peace"[^couch]
+
+... and thus we see that they buried their ***weapons of peace***,
+
+*[revise]*
+
+or they buried ***the weapons of war, for peace***.
+
+[^couch]: Mentioned by Ashely Couch in a commenton Bill's December 21st post.
